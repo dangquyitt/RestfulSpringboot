57448b6577ab025480f3416119f084e40895b906
Nguyen Dang Quy <dangquyitt@gmail.com>
2022-12-07 22:10:44 +0700

    add authorization filter

--- src/main/java/com/junior/Restful/security/CustomSpringSecurity.java
+++ src/main/java/com/junior/Restful/security/CustomSpringSecurity.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -33,7 +34,10 @@ public class CustomSpringSecurity {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .addFilter(getAuthenticationFilter(authenticationManager));
+                .addFilter(getAuthenticationFilter(authenticationManager))
+                .addFilter(new AuthorizationFilter(authenticationManager))
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
     }
 
